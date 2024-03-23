720eee1d1aaed43efc90386ba778ea37da1bb5c2
lonnen <chris.lonnen@gmail.com>
2024-03-23 12:25:28 -0700

    temporary Token types
    Timecode is a known format and could be something like `u8,u8,u8,u16`, while Timespan is just two Timecodes. Figure this out later

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ use chumsky::prelude::*;
 #[derive(Debug)]
 pub enum Token {
     Index(usize),
-    TimeSpan,
-    Timecode,
+    Timespan(String),
+    Timecode(String),
     Text(String)
 }
 
@@ -14,10 +14,9 @@ impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Token::Index(i) => write!(f, "{}", i),
-            Token::TimeSpan => write!(f, "{}"),
-            Token::Timecode => write!(f, "{}"),
+            Token::Timespan(t) => write!(f, "{}", t),
+            Token::Timecode(t) => write!(f, "{}", t),
             Token::Text(s) => write!(f, "{}", s),
-
         }
     }
 }
