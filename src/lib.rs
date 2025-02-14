f4c456c01b766c70f5a50ccd622b3c2cdb902e71
lonnen <chris.lonnen@gmail.com>
2025-02-13 23:58:44 -0800

    refactor to move functionality from main into lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
 use core::fmt;
 
 use chumsky::{prelude::*, text::newline};
+use tower_lsp::{
+    jsonrpc::Result,
+    lsp_types::*, 
+    Client, LanguageServer};
 
 pub type Span = std::ops::Range<usize>;
+
+#[derive(Debug)]
+pub struct Backend {
+    client: Client,
+}
 
 #[derive(Debug, PartialEq)]
 pub struct Timespan {
@@ -100,4 +109,56 @@ pub fn parser() -> impl Parser<char, Vec<(Token, Span)>, Error = Simple<char>> {
         .map_with_span(|tok, span| (tok, span))
         //.padded()
         .repeated()
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            server_info: Some(ServerInfo {
+                name: option_env!("CARGO_PKG_NAME")
+                    .unwrap_or("subtitle-lsp")
+                    .to_string(),
+                version: Some(option_env!("CARGO_PKG_VERSION").unwrap_or("").to_string()),
+            }),
+            capabilities: ServerCapabilities {
+                ..ServerCapabilities::default()
+            },
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "server initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file opened")
+            .await;
+        self.on_change(TextDocumentItem {
+            uri: params.text_document.uri,
+            text: params.text_document.text,
+            version: params.text_document.version,
+            language_id: params.text_document.language_id,
+        })
+        .await
+    }
+}
+
+impl Backend {
+    async fn on_change(&self, _params: TextDocumentItem) {
+        // make a rope from &params.text
+        // stick it into the document_map
+        // attempt to parse &params.text
+        //   package up any parse errors
+        //   send them back to the client
+        // update self.ast_map with whatever ast came back
+        // update semantic_token_map with whatever we got there
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,5 @@
-use tower_lsp::jsonrpc::Result;
-use tower_lsp::lsp_types::*;
-use tower_lsp::{Client, LanguageServer, LspService, Server};
-
-#[derive(Debug)]
-struct Backend {
-    client: Client,
-}
-
-#[tower_lsp::async_trait]
-impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult {
-            server_info: Some(ServerInfo {
-                name: option_env!("CARGO_PKG_NAME")
-                    .unwrap_or("subtitle-lsp")
-                    .to_string(),
-                version: Some(option_env!("CARGO_PKG_VERSION").unwrap_or("").to_string()),
-            }),
-            capabilities: ServerCapabilities {
-                ..ServerCapabilities::default()
-            },
-        })
-    }
-
-    async fn initialized(&self, _: InitializedParams) {
-        self.client
-            .log_message(MessageType::INFO, "server initialized!")
-            .await;
-    }
-
-    async fn shutdown(&self) -> Result<()> {
-        Ok(())
-    }
-
-    async fn did_open(&self, params: DidOpenTextDocumentParams) {
-        self.client
-            .log_message(MessageType::INFO, "file opened")
-            .await;
-        self.on_change(TextDocumentItem {
-            uri: params.text_document.uri,
-            text: params.text_document.text,
-            version: params.text_document.version,
-            language_id: params.text_document.language_id,
-        })
-        .await
-    }
-}
-
-impl Backend {
-    async fn on_change(&self, _params: TextDocumentItem) {
-        // make a rope from &params.text
-        // stick it into the document_map
-        // attempt to parse &params.text
-        //   package up any parse errors
-        //   send them back to the client
-        // update self.ast_map with whatever ast came back
-        // update semantic_token_map with whatever we got there
-    }
-}
+use tower_lsp::{LspService, Server};
+use subtitle_lsp::Backend;
 
 #[tokio::main]
 async fn main() {
@@ -66,7 +8,6 @@ async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::build(|client| Backend { client }).finish();
-
+    let (service, socket) = LspService::new(Backend::new);
     Server::new(stdin, stdout, socket).serve(service).await;
 }
