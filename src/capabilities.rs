d1590e010dd5bbabf792d6e4229827bb759427b1
lonnen <chris.lonnen@gmail.com>
2025-02-17 00:17:09 -0800

    add commented out server capability
    this is one of several planned future capabilities

--- src/capabilities.rs
+++ src/capabilities.rs
@@ -2,6 +2,15 @@ use tower_lsp::lsp_types::*;
 
 pub fn server_capabilities(client_capabilities: &ClientCapabilities) -> ServerCapabilities {
     ServerCapabilities {
+        // text_document_sync: Some(TextDocumentSyncCapability::Options(
+        //     TextDocumentSyncOptions {
+        //         open_close: Some(true),
+        //         change: Some(TextDocumentSyncKind::INCREMENTAL),
+        //         will_save: None,
+        //         will_save_wait_until: None,
+        //         save: Some(SaveOptions::default().into()),
+        //     },
+        // )),
         ..Default::default()
     }
 }
