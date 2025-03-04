11a07a6caf28fcd184270464cf956c8968fb59a1
lonnen <chris.lonnen@gmail.com>
2025-03-04 16:05:48 -0600

    remove unused test
    the sole test is not providing much value and the test suite is totally ignored, pass or fail

--- tests/test-lib.rs
+++ tests/test-lib.rs
@@ -4,14 +4,3 @@ const SIMPLE_SRT: &str = "
 This is an example of
 a subtitle.
 ";
-
-#[test]
-fn test_lex() {
-    let (tokens, _) = parser().parse_recovery(SIMPLE_SRT);
-    let tokens_unwrapped = tokens.unwrap();
-    assert_eq!(tokens_unwrapped.len(), 0);
-    for (token, _) in tokens_unwrapped {
-        assert_eq!(token, Token::Text("stuff".to_string()));
-        println!("{}", token);
-    }
-}
